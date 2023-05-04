d6113b42312ae4573819efb4492087853666e1cf
enwadiogbu <emekanwadiogbu.jr@me.com>
2023-05-04 00:46:18 -0700

    Second commit, implementing the Navigation, Footer, Hero image, and CSS with animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Profile from "./PortfolioContainer/Home/Profile";
-import Footer from './PortfolioContainer/Home/Footer/Footer';
+import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
+
 function App() {
   return (
     <div className="App">
-      <Profile />
-      <Footer />
+        <PortfolioContainer />
 
     </div>
   );
